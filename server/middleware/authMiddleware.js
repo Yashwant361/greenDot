512fb7fece76478f1024df3d0c4927b47c6fd70a
yashwantray361 <yashwantray361@gmail.com>
2026-05-18 17:32:33 +0530

    fix studentModels

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+
+    try {
+
+        const authHeader =
+            req.headers.authorization;
+
+        if (!authHeader) {
+
+            return res.status(401).json({
+                message: "No token provided"
+            });
+
+        }
+
+        const token =
+            authHeader.split(" ")[1];
+            
+        // console.log(token);
+        // console.log(process.env.JWT_SECRET);
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        req.user = decoded;
+
+        next();
+
+    } catch (error) {
+
+        res.status(401).json({
+            message: "Invalid token"
+        });
+
+    }
+
+};
+
+module.exports = authMiddleware;
